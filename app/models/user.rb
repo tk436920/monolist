cab4728fb77011762db1e7c7e818c91d6b8c2c63
tk436920 <tk436920@gmail.com>
2015-10-22 08:04:19 +0000

    pre have want

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,11 @@ class User < ActiveRecord::Base
   has_many :followed_users, through: :followed_relationships, source: :follower
 
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
-  has_many :items ,through: :ownerships
-
+  has_many :items, through: :ownerships
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items, through: :wants, source: :item
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items, through: :haves, source: :item
 
   # 他のユーザーをフォローする
   def follow(other_user)
@@ -31,20 +34,26 @@ class User < ActiveRecord::Base
 
   ## TODO 実装
   def have(item)
+    ownerships.create(item_id: item.id)
   end
 
   def unhave(item)
+    ownerships.find_by(item_id: item.id).destroy
   end
 
   def have?(item)
+    have_items.include?(item)
   end
 
   def want(item)
+    ownerships.create(item_id: item.id)
   end
 
   def unwant(item)
+    ownerships.find_by(item_id: item.id).destroy
   end
 
   def want?(item)
+    want_items.include?(item)
   end
 end
